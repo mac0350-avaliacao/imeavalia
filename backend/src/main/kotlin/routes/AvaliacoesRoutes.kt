bf45b249ff1c0e96813b15925a4d6d5e7afd9f61
ayameira-loop <aya.meira@looplex.com.br>
2024-06-06 23:30:08 -0500

    fix(route) Checking whether oferecimento exists before creating a new avaliacao entry

--- backend/src/main/kotlin/routes/AvaliacoesRoutes.kt
+++ backend/src/main/kotlin/routes/AvaliacoesRoutes.kt
@@ -6,6 +6,7 @@ import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import models.*
+import org.jetbrains.exposed.sql.Op
 
 fun Route.avaliacoesRoutes() {
     route("/avaliacoes") {
@@ -20,19 +21,41 @@ fun Route.avaliacoesRoutes() {
 
         post {
             val avaliacao = call.receive<Avaliacao>()
-            val newAvaliacao = transaction {
-                AvaliacaoEntity.new {
-                    disciplinaId = avaliacao.disciplinaId
-                    materialDidatico = avaliacao.materialDidatico
-                    didaticaProfessor = avaliacao.didaticaProfessor
-                    metodoAvaliativo = avaliacao.metodoAvaliativo
-                    monitoria = avaliacao.monitoria
-                    comentariosGerais = avaliacao.comentariosGerais
-                    presencaAtividades = avaliacao.presencaAtividades
-                    horasSemanais = avaliacao.horasSemanais
+
+            val oferecimentos = transaction {
+                OferecimentoEntity.all().map {
+                    it.toOferecimento()
                 }
             }
-            call.respond(newAvaliacao.toAvaliacao())
+
+            val oferecimento = oferecimentos.find { it.disciplinaId == avaliacao.disciplinaId && it.anoSemestre == avaliacao.anoSemestre }
+
+            if (oferecimento != null) {
+                val newAvaliacao = transaction {
+                    AvaliacaoEntity.new {
+                        disciplinaId = avaliacao.disciplinaId
+                        anoSemestre = avaliacao.anoSemestre
+                        oferecimentoId = avaliacao.oferecimentoId
+                        materialDidatico = avaliacao.materialDidatico
+                        didaticaProfessor = avaliacao.didaticaProfessor
+                        metodoAvaliativo = avaliacao.metodoAvaliativo
+                        monitoria = avaliacao.monitoria
+                        comentariosGerais = avaliacao.comentariosGerais
+                        presencaAtividades = avaliacao.presencaAtividades
+                        horasSemanais = avaliacao.horasSemanais
+                    }
+                }
+                call.respond(newAvaliacao.toAvaliacao())
+            } else {
+                val newOferecimento = transaction {
+                    OferecimentoEntity.new {
+                        disciplinaId
+                        professorId
+                        anoSemestre
+                    }
+                }
+                call.respond(newOferecimento.toOferecimento())
+            }
         }
     }
 }
