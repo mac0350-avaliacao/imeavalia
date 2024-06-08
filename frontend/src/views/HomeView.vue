57fe4b58621dab0807cb494f038feef5f1db729a
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-06-08 16:51:39 -0300

    change style of the mainpage
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,79 +1,75 @@
 <script setup></script>
 
 <template>
-  <main>
-    <div>
-      <div class="disciplineSelect">
-        <v-select
-          v-model="selectedDisciplines"
-          :items="disciplines"
-          item-title="nome"
-          item-value="id"
-          hint="Selecione todas as matérias que você irá avaliar"
-          label="Selecione a disciplina"
-          persistent-hint
-        ></v-select>
-      </div>
-      <div class="professorSelect">
-        <v-select
-            v-model="selectedProfessors"
-            :items="professors"
-            item-title="nome"
-            item-value="id"
-            label="Selecione o(a) professor(a)"
-            persistent-hint
-        ></v-select>
-      </div>
-      <div class="disciplineReview">
-        <div v-for="type in reviewTypes" :key="type">
-          <p>Avaliação do {{ type.name }}: {{ type.value }}</p>
-          <v-radio-group v-model="type.value">
-            <v-radio :color="reviewColors[0]" label="Muito ruim" value="MuitoRuim"></v-radio>
-            <v-radio :color="reviewColors[1]" label="Ruim" value="Ruim"></v-radio>
-            <v-radio label="Neutro" value="neutro"></v-radio>
-            <v-radio :color="reviewColors[2]" label="Bom" value="Bom"></v-radio>
-            <v-radio :color="reviewColors[3]" label="Muito bom" value="MuitoBom"></v-radio>
-          </v-radio-group>
-        </div>
-      </div>
-      <div>
-        <p>
-          Quantas horas por semana você dedica ao estudo dessa disciplina? <br />
-        </p>
-        <v-radio-group v-model="disciplineHours">
-          <v-radio label="1h ou menos" value="UmaHoraOuMenos"></v-radio>
-          <v-radio label="2h" value="DuasHoras"></v-radio>
-          <v-radio label="3 horas" value="TresHoras"></v-radio>
-          <v-radio label="4 horas ou mais" value="QuatroHorasOuMais"></v-radio>
+  <main class="home">
+    <div class="disciplineSelect">
+      <v-select
+        v-model="selectedDisciplines"
+        :items="disciplines"
+        item-title="nome"
+        item-value="id"
+        hint="Selecione todas as matérias que você irá avaliar"
+        label="Selecione a disciplina"
+        persistent-hint
+      ></v-select>
+    </div>
+    <div class="professorSelect">
+      <v-select
+        v-model="selectedProfessors"
+        :items="professors"
+        item-title="nome"
+        item-value="id"
+        label="Selecione o(a) professor(a)"
+        persistent-hint
+      ></v-select>
+    </div>
+    <div class="disciplineReview">
+      <div v-for="type in reviewTypes" :key="type">
+        <p>Avaliação do {{ type.name }}</p>
+        <v-radio-group v-model="type.value">
+          <v-radio :color="reviewColors['veryBad']" label="Muito ruim" value="MuitoRuim"></v-radio>
+          <v-radio :color="reviewColors['bad']" label="Ruim" value="Ruim"></v-radio>
+          <v-radio label="Neutro" value="neutro"></v-radio>
+          <v-radio :color="reviewColors['good']" label="Bom" value="Bom"></v-radio>
+          <v-radio :color="reviewColors['veryGood']" label="Muito bom" value="MuitoBom"></v-radio>
         </v-radio-group>
-      </div>
-      <div>
-        <p>
-          Quanto a sua presença nas aulas e realização de atividades propostas, você diria que
-          <br />
-        </p>
-        <v-radio-group v-model="disciplinePresence">
-          <v-radio label="muito ruim" value="MuitoRuim"></v-radio>
-          <v-radio label="ruim" value="Ruim"></v-radio>
-          <v-radio label="boa" value="Bom"></v-radio>
-          <v-radio label="muito boa" value="MuitoBom"></v-radio>
-        </v-radio-group>
-      </div>
-      <div>
-        <v-text-field v-model="comentariosGerais" label="Comentários gerais"></v-text-field>
-      </div>
-      <div>
-        <v-text-field v-model="comentariosAvaliacao" label="Comentários consulta"></v-text-field>
-      </div>
-      <div>
-        <v-btn @click="submitForm">Enviar</v-btn>
       </div>
     </div>
+    <div>
+      <p>Quantas horas por semana você dedica ao estudo dessa disciplina? <br /></p>
+      <v-radio-group v-model="disciplineHours">
+        <v-radio label="1h ou menos" value="UmaHoraOuMenos"></v-radio>
+        <v-radio label="2h" value="DuasHoras"></v-radio>
+        <v-radio label="3 horas" value="TresHoras"></v-radio>
+        <v-radio label="4 horas ou mais" value="QuatroHorasOuMais"></v-radio>
+      </v-radio-group>
+    </div>
+    <div>
+      <p>
+        Quanto a sua presença nas aulas e realização de atividades propostas, você diria que
+        <br />
+      </p>
+      <v-radio-group v-model="disciplinePresence">
+        <v-radio :color="reviewColors['veryBad']" label="muito ruim" value="MuitoRuim"></v-radio>
+        <v-radio :color="reviewColors['bad']" label="ruim" value="Ruim"></v-radio>
+        <v-radio :color="reviewColors['good']" label="boa" value="Bom"></v-radio>
+        <v-radio :color="reviewColors['veryGood']" label="muito boa" value="MuitoBom"></v-radio>
+      </v-radio-group>
+    </div>
+    <div>
+      <p>Caso queira, escreva comentários gerais sobre a matéria</p>
+      <v-textarea v-model="comentariosGerais" label="Comentários gerais"></v-textarea>
+    </div>
+    <div>
+      <p>Caso queira, escreva comentários sobre a consulta</p>
+      <v-textarea v-model="comentariosAvaliacao" label="Comentários consulta"></v-textarea>
+    </div>
+    <v-btn @click="submitForm">Enviar</v-btn>
   </main>
 </template>
-<script>
 
-import api from '../api';
+<script>
+import api from '../api'
 
 export default {
   data: () => ({
@@ -82,7 +78,13 @@ export default {
     anoSemestre: '',
     selectedDisciplines: null,
     selectedProfessors: null,
-    reviewColors: ['red', '#DC143C', 'green', 'blue'],
+    //reviewColors: ['red', '#DC143C', 'green', 'blue'],
+    reviewColors: {
+      veryBad: 'red',
+      bad: '#DC143C',
+      good: 'green',
+      veryGood: 'blue'
+    },
     professors: [],
     disciplines: [],
     reviewTypes: [
@@ -96,71 +98,79 @@ export default {
   }),
   async created() {
     try {
-      const response = await api.getDisciplinas();
-      this.disciplines = response.data;
+      const response = await api.getDisciplinas()
+      this.disciplines = response.data
     } catch (error) {
-      console.error('Error fetching disciplines:', error);
+      console.error('Error fetching disciplines:', error)
     }
 
     try {
-      const response = await api.getProfessores();
-      this.professors = response.data;
+      const response = await api.getProfessores()
+      this.professors = response.data
     } catch (error) {
-      console.error('Error fetching professors:', error);
+      console.error('Error fetching professors:', error)
     }
   },
   mounted() {
-    this.setYearSemester();
+    this.setYearSemester()
   },
   methods: {
     setYearSemester() {
-      const current_date = new Date();
+      const current_date = new Date()
 
-      const current_year = current_date.getFullYear();
-      const current_month = current_date.getMonth();
+      const current_year = current_date.getFullYear()
+      const current_month = current_date.getMonth()
 
-      var semester = current_month <= 5 ? 1 : 2; 
+      var semester = current_month <= 5 ? 1 : 2
 
-      this.anoSemestre = `${current_year}-${semester}`;
+      this.anoSemestre = `${current_year}${semester}`
     },
+
     async submitForm() {
       const formData = {
         disciplinaId: this.selectedDisciplines,
         professorId: this.selectedProfessors,
         anoSemestre: this.anoSemestre,
-        materialDidatico: this.reviewTypes.find(type => type.name === 'Material Didático').value,
-        didaticaProfessor: this.reviewTypes.find(type => type.name === 'Didática do professor').value,
-        metodoAvaliativo: this.reviewTypes.find(type => type.name === 'Método avaliativo').value,
-        monitoria: this.reviewTypes.find(type => type.name === 'Monitoria').value,
+        materialDidatico: this.reviewTypes.find((type) => type.name === 'Material Didático').value,
+        didaticaProfessor: this.reviewTypes.find((type) => type.name === 'Didática do professor')
+          .value,
+        metodoAvaliativo: this.reviewTypes.find((type) => type.name === 'Método avaliativo').value,
+        monitoria: this.reviewTypes.find((type) => type.name === 'Monitoria').value,
         horasSemanais: this.disciplineHours,
         presencaAtividades: this.disciplinePresence,
         comentariosGerais: this.comentariosGerais,
         comentariosAvaliacao: this.comentariosAvaliacao
-      };
+      }
 
       console.log(formData)
 
-      const pl = {
-        "disciplinaId": 12,
-        "anoSemestre": 20212,
-        "materialDidatico": "Bom",
-        "didaticaProfessor": "MuitoBom",
-        "metodoAvaliativo": "Neutro",
-        "monitoria": "Ruim",
-        "comentariosGerais": "da aaaaaa aaaaaaaaaa aaaaaa asdasdasd",
-        "presencaAtividades": "Bom",
-        "horasSemanais": "DuasHoras"
-      }
-
       try {
-        const response = await api.postAvaliacao(formData);
-        console.log('Form submitted successfully:', response.data);
+        const response = await api.postAvaliacao(formData)
+        console.log('Form submitted successfully:', response.data)
       } catch (error) {
-        console.error('Error submitting form:', error);
+        console.error('Error submitting form:', error)
       }
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+.disciplineReview {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.disciplineReview > * {
+  padding: 1em;
+}
+
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #e3f3f3;
+  border-radius: 1em;
+  padding: 2em;
+}
+</style>
