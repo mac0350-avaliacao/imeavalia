12ede2d57d682c71c78752a6e060273947e75113
ayameira-loop <aya.meira@looplex.com.br>
2024-06-16 08:26:34 -0500

    feat(init function) Creating a function to initialize the databases

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.11"
+    id("com.github.johnrengelman.shadow") version "2.0.4"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "br.com.imeavalia"
@@ -10,7 +12,11 @@ repositories {
     mavenCentral()
 }
 
+project.setProperty("mainClassName", "br.com.imeavalia.Main")
+
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
     // Ktor
     implementation("io.ktor:ktor-server-core:2.3.11")
     implementation("io.ktor:ktor-serialization-gson:2.3.11")
@@ -21,9 +27,9 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests:2.3.11")
 
     // Exposed framework
-    implementation("org.jetbrains.exposed:exposed-core:0.35.3")
-    implementation("org.jetbrains.exposed:exposed-dao:0.35.3")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.35.3")
+    implementation("org.jetbrains.exposed:exposed-core:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
 
     // PostgresSQL JDBC Driver
     implementation("org.postgresql:postgresql:42.3.3")
